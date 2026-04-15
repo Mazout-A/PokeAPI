f0c3eb7a09e55ad1de8fede2c8ff6b88f4d1debb
Billal Boudlal <Billalboudlal2210@outlook.fr>
2026-04-15 21:53:07 +0200

    ajout dans pokemon.js les chaine d'évolution des pokemons

--- src/js/pokemon.js
+++ src/js/pokemon.js
@@ -9,3 +9,23 @@ export async function fetchPokemonDetail(url) {
   const pokemon = await detail.json();
   return pokemon;
 }
+
+export async function eveloutionChain(pokemonId) {
+  const res1 = await fetch(
+    `https://pokeapi.co/api/v2/pokemon-species/${pokemonId}`,
+  );
+  const species = await res1.json();
+
+  const res2 = await fetch(species.evolution_chain.url);
+  const chain = await res2.json();
+
+  const evolutions = [];
+  let current = chain.chain;
+
+  while (current) {
+    evolutions.push(current.species.name);
+    current = current.evolves_to[0] || null;
+  }
+
+  return evolutions;
+}
